5c2abe5b5fc7d88058e9d7aacf77bf9f4e1f3108
Lucas Castro <professorlucascastro@gmail.com>
2021-12-30 12:00:23 -0300

    Adicionado projeto Spring Boot

---
+++ src/main/java/com/github/lucascastrodev/projetoFinalTqi/ProjetoFinalTqiApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucascastrodev.projetoFinalTqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalTqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalTqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucascastrodev/projetoFinalTqi/ProjetoFinalTqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucascastrodev.projetoFinalTqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalTqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
